a6608ca42a67b92cc71f0c8feda48825fdd78884
Sunil Gopinath <sxg282@gmail.com>
2012-11-11 21:46:49 -0500

    added restkit, it can be done

---
+++ SampleRestKit/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  SampleRestKit
+//
+//  Created by Sunil Gopinath on 11/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <RestKit/RestKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
